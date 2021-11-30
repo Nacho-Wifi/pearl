8b4000a9735f34985391aa3fa96f354511dce5b1
Kah Yan Lee <kahyanlee6@gmail.com>
2021-11-30 14:04:11 -0600

    feat: takes a picture and displays picture with option to retake or save photo

--- screens/ImageEntries.js
+++ screens/ImageEntries.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import ImagePreview from './ImagePreview';
 
 const ImageEntries = () => {
-  let camera = Camera;
+  let cameraRef = useRef(null);
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [preview, setPreview] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -20,65 +23,75 @@ const ImageEntries = () => {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  const __takePicture = async () => {
+  const takePicture = async () => {
     if (!hasPermission) return;
-    const photo = await camera.takePictureAsync();
+    //returns an object containing information about the photo, including uri
+    const photo = await cameraRef.current.takePictureAsync();
+    setPreview(true);
+    setCapturedImage(photo);
   };
+  const savePhoto = () => {};
 
+  const retakePhoto = () => {
+    setCapturedImage(null);
+    setPreview(false);
+  };
   return (
     <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={type}
-        ref={(ref) => {
-          camera = ref;
-        }}
-      >
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          >
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            flexDirection: 'row',
-            flex: 1,
-            width: '100%',
-            padding: 20,
-            justifyContent: 'space-between',
-          }}
-        >
+      {preview && capturedImage ? (
+        <ImagePreview
+          photo={capturedImage}
+          savePhoto={savePhoto}
+          retakePhoto={retakePhoto}
+        />
+      ) : (
+        <Camera style={styles.camera} type={type} ref={cameraRef}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}
+            >
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+          </View>
           <View
             style={{
-              alignSelf: 'center',
+              position: 'absolute',
+              bottom: 0,
+              flexDirection: 'row',
               flex: 1,
-              alignItems: 'center',
+              width: '100%',
+              padding: 20,
+              justifyContent: 'space-between',
             }}
           >
-            <TouchableOpacity
-              onPress={__takePicture}
+            <View
               style={{
-                width: 70,
-                height: 70,
-                bottom: 0,
-                borderRadius: 50,
-                backgroundColor: '#fff',
+                alignSelf: 'center',
+                flex: 1,
+                alignItems: 'center',
               }}
-            />
+            >
+              <TouchableOpacity
+                onPress={takePicture}
+                style={{
+                  width: 70,
+                  height: 70,
+                  bottom: 0,
+                  borderRadius: 50,
+                  backgroundColor: '#fff',
+                }}
+              />
+            </View>
           </View>
-        </View>
-      </Camera>
+        </Camera>
+      )}
     </View>
   );
 };
---
+++ screens/ImagePreview.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+
+const ImagePreview = ({ photo, retakePhoto, savePhoto }) => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <ImageBackground
+        source={{ uri: photo && photo.uri }}
+        style={{
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            padding: 15,
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <TouchableOpacity
+              onPress={retakePhoto}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4,
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20,
+                }}
+              >
+                Re-take
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={savePhoto}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4,
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20,
+                }}
+              >
+                save photo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default ImagePreview;
+
+const styles = StyleSheet.create({});
