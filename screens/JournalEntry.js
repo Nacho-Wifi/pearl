55682d0bd9e2dcd877b4ab9d773b43cdf5a53476
Kah Yan Lee <kahyanlee6@gmail.com>
2021-12-03 14:06:56 -0600

    feat: abstract setJournal logic from mood button

--- screens/JournalEntry.js
+++ screens/JournalEntry.js
@@ -35,6 +35,7 @@ const JournalEntry = ({ route }) => {
   const [textEntry, setTextEntry] = useState(false);
   const [userActivities, setUserActivities] = useState([]);
   const [userJournalData, setUserJournalData] = useState(null);
+  const [selectedMood, setSelectedMood] = useState({});
   const moodsCollectionRef = collection(db, 'Moods');
   const journalsCollectionRef = collection(db, 'Journals');
   useEffect(() => {
@@ -51,6 +52,7 @@ const JournalEntry = ({ route }) => {
     //so it doesn't get lost when we nagivate to the Textentries page
     if (journalData) {
       setUserJournalData(journalData);
+      setSelectedMood(journalData.journalEntries.mood);
     }
   }, []);
 
@@ -58,7 +60,7 @@ const JournalEntry = ({ route }) => {
   //to toggle between adding text entry and edit text entry
   useEffect(() => {
     // if a photo url or input text exists locally or in the database, set setTextEntry to true
-    // TODO: 
+    // TODO:
     if (photoURI || inputText) {
       setTextEntry(true);
     } else setTextEntry(false);
@@ -71,8 +73,12 @@ const JournalEntry = ({ route }) => {
   const handleOptionalEntry = () => {
     navigation.navigate('TextEntry', {
       // if userJournalData is defined, we want to pass down userJournalData.journalEntries.photoURL
-      photoURI: userJournalData ? userJournalData.journalEntries.photoURL : photoURI, // Allows user to view photo when toggling back and forth between JournalEntry and TextEntry
-      inputText: userJournalData ? userJournalData.journalEntries.textInput : inputText,
+      photoURI: userJournalData
+        ? userJournalData.journalEntries.photoURL
+        : photoURI, // Allows user to view photo when toggling back and forth between JournalEntry and TextEntry
+      inputText: userJournalData
+        ? userJournalData.journalEntries.textInput
+        : inputText,
     });
   };
 
@@ -111,7 +117,7 @@ const JournalEntry = ({ route }) => {
     // If journalId is undefined, create a new journal entry
     if (!userJournalData) {
       await addDoc(journalsCollectionRef, {
-        mood,
+        mood: selectedMood,
         activities: userActivities,
         photoURL: downloadURL || '',
         textInput: inputText || '',
@@ -140,20 +146,20 @@ const JournalEntry = ({ route }) => {
         return (
           <TouchableOpacity
             key={mood.id}
-            style={styles.button}
+            style={
+              selectedMood.id === mood.id
+                ? styles.selectedButton
+                : styles.button
+            }
             onPress={() => {
-              if (photoURI) {
-                //will call setJournal in savePhoto after getting downloadURL
-                savePhoto(mood);
-              } else {
-                setJournal(mood);
-              }
+              setSelectedMood(mood);
             }}
           >
             <Text>{emojiMapping[mood.imageUrl]}</Text>
           </TouchableOpacity>
         );
       })}
+
       {!textEntry ? (
         <TouchableOpacity style={styles.button} onPress={handleOptionalEntry}>
           <Text>Add Text Entry</Text>
@@ -163,6 +169,19 @@ const JournalEntry = ({ route }) => {
           <Text>Edit Text Entry</Text>
         </TouchableOpacity>
       )}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          if (photoURI) {
+            //will call setJournal in savePhoto after getting downloadURL
+            savePhoto(selectedMood);
+          } else {
+            setJournal(selectedMood);
+          }
+        }}
+      >
+        <Text>Save Journal</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -175,14 +194,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  // button: {
+  //   backgroundColor: '#BDD8F1',
+  //   width: '24%',
+  //   padding: 15,
+  //   borderRadius: 10,
+  //   margin: 16,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
   button: {
     backgroundColor: '#BDD8F1',
-    width: '24%',
+    width: '60%',
     padding: 15,
-    borderRadius: 10,
     margin: 16,
-    justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 10,
+  },
+  selectedButton: {
+    backgroundColor: '#7bb6ed', // Temporary!
+    width: '60%',
+    margin: 16,
+    padding: 15,
+    alignItems: 'center',
+    // borderColor: '#BDD8F1',
+    // borderWidth: 2,
+    borderRadius: 10,
   },
   // buttonText: {
   //   color: 'white',
