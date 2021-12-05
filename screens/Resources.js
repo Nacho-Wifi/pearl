c93ca10d47fcce4f53588e5afdc044ae86463732
Caroline Spiezio <spieziocaroline@gmail.com>
2021-12-04 18:09:22 -0800

    map getting return values from search

--- screens/Resources.js
+++ screens/Resources.js
@@ -1,8 +1,59 @@
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Dimensions } from 'react-native';
+import {
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import * as Location from 'expo-location';
+import { GOOGLE_MAPS_API_KEY, GOOGLE_PLACES_API } from '@env';
+
+const getPlaces = async (type, location) => {
+  console.log('I GO TO LOUUUUUD PLACEEEES', location);
+  let radius = 1000;
+
+  const nearbyService = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
+  const queryBase = `https://maps.googleapis.com/maps/api/place/textsearch/json?`;
+  const api = `&key=${GOOGLE_PLACES_API}`;
+
+  const locationUrl = `location=${location.coords.latitude},${location.coords.longitude}`;
+  const typeData = `&type=${type}`;
+
+  let url = `${nearbyService}${locationUrl}&rankby=distance${typeData}${api}`;
+  console.log('DO WE HIT THIS????', url);
+
+  let newPromise;
+  newPromise = new Promise((res, rej) => {
+    res(
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          return res.results.map((element) => {
+            console.log('ELEMENT!!!!', element);
+            return {
+              id: element.place_id,
+              name: element.name,
+              rating: element.rating,
+              vicinity: element.vicinity,
+              marker: {
+                latitude: element.geometry.location.lat,
+                longitude: element.geometry.location.lng,
+              },
+            };
+          });
+        })
+        .then((value) => {
+          return value;
+        })
+    );
+  });
+  return newPromise;
+};
 
 const Map = () => {
   const [location, setLocation] = useState(null);
@@ -57,6 +108,10 @@ const Map = () => {
       return (
         <View style={styles.container}>
           <Text>Resources Near You</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => getPlaces('park', location)}
+          ></TouchableOpacity>
           <MapView
             style={styles.map}
             provider={PROVIDER_GOOGLE}
@@ -93,6 +148,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#FBD1B7',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   map: {
     width: '90%',
