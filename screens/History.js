319bfb7e9074e80476a4fa80e9738aa0cb513d30
Kah Yan Lee <kahyanlee6@gmail.com>
2021-12-04 18:37:51 -0600

    feat: adds calendar to display

--- screens/History.js
+++ screens/History.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import ModalHistory from './ModalHistory';
+import { auth, db } from '../firebase';
+import { getDocs, query, where, collection } from 'firebase/firestore';
 
-const HelloWorldApp = () => {
+const History = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [entry, setEntry] = useState({});
+  const journalEntriesCollectionRef = collection(db, 'Journals');
+  const handleClick = async (daySelected) => {
+    console.log('i am daySelected', daySelected);
+    const userId = auth.currentUser.email;
+    const entryQuery = query(
+      journalEntriesCollectionRef,
+      where('userId', '==', userId),
+      where('createdAt', '==', daySelected)
+    );
+    const querySnapshot = await getDocs(entryQuery);
+    querySnapshot.forEach((doc) => {
+      //setEntry as the doc we get back from Journal for that day
+      //will pass down to ModalHistory as props to display
+      setEntry(doc.data());
+    });
+    setModalVisible(true);
+  };
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
       }}
     >
-      <Text>History!</Text>
-    </View>
+      <Calendar
+        onDayPress={(day) => {
+          //we have access to the datestring aka yyyy-mm-dd.
+          //to show the correct date when using toDateString(), we must change the format
+          //from yyyy-mm-dd to yyyy/mm/dd
+          console.log(
+            'selected day',
+            new Date(day.dateString.replace(/-/g, '/')).toDateString()
+          );
+          handleClick(
+            new Date(day.dateString.replace(/-/g, '/')).toDateString()
+          );
+        }}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        maxDate={new Date()}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={(subtractMonth) => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={(addMonth) => addMonth()}
+      />
+      {modalVisible && (
+        <ModalHistory
+          setModalVisible={setModalVisible}
+          modalVisible={modalVisible}
+          entry={entry}
+        />
+      )}
+    </SafeAreaView>
   );
 };
-export default HelloWorldApp;
+
+export default History;
+
+const styles = StyleSheet.create({});
---
+++ screens/ModalHistory.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
+
+const ModalHistory = ({ setModalVisible, modalVisible, entry }) => {
+  //check if entry is empty object aka
+  //no journal entry for the date selected
+  console.log('i am the etrt', entry);
+  if (Object.keys(entry).length === 0) {
+    console.log('i am in here');
+    return <View>Sorry, you didn't have an Entry for this day</View>;
+  }
+  return (
+    <View style={styles.modalContainer}>
+      <Modal
+        animationType="slide"
+        // transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Mood:</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default ModalHistory;
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    position: 'absolute',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
